8a672a4d4767a3c8b698fcf11c6f3497389042c1
AndresE65 <scarpenter1105.29@gmail.com>
2020-06-07 19:41:18 -0500

    SATTAvance 07.06.20

---
+++ Assets/Menu/menuprincipal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuprincipal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EmpezarJuego()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void CerrarJuego()
+    {
+        Application.Quit();
+        Debug.Log("Salir");
+    }
+}
